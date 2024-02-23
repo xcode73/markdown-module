bcbf089672b8c890ff5130f712122c9ea6798d34
xcode73 <xcode.uln@gmail.com>
2024-02-23 16:13:07 +0400

    swift-tools-version:5.9

--- Package.swift
+++ Package.swift
@@ -1,25 +1,26 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "markdown-module",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "MarkdownModule", targets: ["MarkdownModule"]),
+        .library(name: "MarkdownModule", targets: ["MarkdownModule"])
     ],
     dependencies: [
-        .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/JohnSundell/Ink", from: "0.6.0"),
+        .package(url: "https://github.com/xcode73/feather-core", branch: "test-dev"),
+        .package(url: "https://github.com/JohnSundell/Ink", from: "0.6.0")
     ],
     targets: [
         .target(name: "MarkdownModule", dependencies: [
             .product(name: "Feather", package: "feather-core"),
-            .product(name: "Ink", package: "Ink"),
+            .product(name: "Ink", package: "Ink")
         ],
         resources: [
-//            .copy("Bundle"),
+//            .copy("Bundle")
         ]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
