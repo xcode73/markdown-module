8ad91ed823a72064219dd12902ef05a747689b92
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-06-07 10:37:43 +0400

    build: update dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../feather-core"),
-//        .package(url: "https://github.com/xcode73/feather-core.git", branch: "main"),
+//      .package(url: "https://github.com/xcode73/feather-core.git", branch: "main"),
         .package(url: "https://github.com/JohnSundell/Ink.git", from: "0.6.0"),
     ],
     targets: [
